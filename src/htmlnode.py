5dc9132e5191d2cd9f65e7264636870f0ad96ed1
SirSidge <heinz.hscharlach@gmail.com>
2025-01-28 19:16:00 +0200

    Added LeafNode and tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -13,8 +13,22 @@ class HTMLNode():
             return ""
         new_string = ""
         for key, value in self.props.items():
-            new_string += " " + f"{key}= {value}"
+            new_string += " " + f"{key}={value}"
         return new_string
 
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value is None or self.value == "":
+            raise ValueError("Text is required")
+        if self.tag is None or self.tag == "":
+            return  self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,19 +1,19 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_html(self):
         node = HTMLNode("p", "This is normal text", None, {"href": "https://www.google.com"})
         self.assertEqual(
-            " href= https://www.google.com",
+            " href=https://www.google.com",
             node.props_to_html()
         )
     
     def test_few_props(self):
         node = HTMLNode("p", "This is normal text", None, {"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(
-            " href= https://www.google.com target= _blank",
+            " href=https://www.google.com target=_blank",
             node.props_to_html()
         )
     
@@ -21,5 +21,29 @@ class TestHTMLNode(unittest.TestCase):
         node = HTMLNode("div", "Text as usual", None, {'href': 'https://www.google.com'})
         self.assertEqual(
             "HTMLNode(div, Text as usual, children: None, {'href': 'https://www.google.com'})",
+            node.__repr__()
+        )
+
+    def test_leaf(self):
+        node = LeafNode("p", "I hope Boot.dev is the answer.", {'href': 'https://www.boot.dev'})
+        self.assertEqual(
+            "<p href=https://www.boot.dev>I hope Boot.dev is the answer.</p>",
+            node.to_html()
+        )
+        #Just text
+        node2 = LeafNode("", "Text without tag", {})
+        self.assertEqual(
+            "Text without tag",
+            node2.to_html()
+        )
+        #Value error if no text given
+        node3 = LeafNode("p", "", {'href': 'https://www.boot.dev'})
+        with self.assertRaises(ValueError):
+            node3.to_html()
+    
+    def test_leaf_repr(self):
+        node = LeafNode("p", "I hope Boot.dev is the answer.", {'href': 'https://www.boot.dev'})
+        self.assertEqual(
+            "LeafNode(p, I hope Boot.dev is the answer., {'href': 'https://www.boot.dev'})",
             node.__repr__()
         )
